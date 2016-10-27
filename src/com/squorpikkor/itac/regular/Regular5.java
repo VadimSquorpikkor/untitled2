77e881e1fd4e530f45d73795b8b82b13c8c576d0
VadimSquorpikkor <uniqdela@gmail.com>
2016-10-28 00:32:07 +0300

    2

--- src/com/squorpikkor/itac/regular/Regular5.java
+++ src/com/squorpikkor/itac/regular/Regular5.java
@@ -7,9 +7,9 @@ import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by SquorpikkoR on 23.10.2016.
- */
+
+ //Created by SquorpikkoR on 23.10.2016.
+
 public class Regular5 {
     public static void main(String[] args) throws IOException {
         System.out.print("enter your regex: ");
